a114ac905bc9f7b41a660ff9f91acea088a2506b
kalbatati <khdojabat18@gmail.com>
2024-05-15 17:29:24 +1000

    Initial Commit

---
+++ FriendApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FriendApp
+//
+//  Created by user249683 on 5/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
